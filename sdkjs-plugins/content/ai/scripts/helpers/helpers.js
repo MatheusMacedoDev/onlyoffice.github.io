8b637ae20396d38ffefee8f643c44d1baf35cd5c
Matheus <matheus.macedo.developer@gmail.com>
2026-09-05 21:51:29 -0300

    Update AI plugin to v3.3.2

--- sdkjs-plugins/content/ai/scripts/helpers/helpers.js
+++ sdkjs-plugins/content/ai/scripts/helpers/helpers.js
@@ -7294,9 +7294,12 @@ HELPERS.cell.push((function () {
         if (typeof range !== "string" || !range.trim())
             return null;
 
+        Asc.scope.range = range.trim();
+
         return await Asc.Editor.callCommand(function () {
+            debugger;
             const worksheet = Api.GetActiveSheet();
-            const headerRange = worksheet.GetRange(range);
+            const headerRange = worksheet.GetRange(Asc.scope.range);
 
             if (!headerRange)
                 return null;
@@ -7323,14 +7326,15 @@ HELPERS.cell.push((function () {
         Asc.scope.colCount = header.fields.length;
 
         await Asc.Editor.callCommand(function () {
+            debugger;
             const worksheet = Api.GetActiveSheet();
             const headerRange = worksheet.GetRange(Asc.scope.address);
             const fillRange = headerRange.Resize(Asc.scope.rowCount + 1, Asc.scope.colCount);
 
-            for (let rowIndex = 1; rowIndex <= Asc.scope.rowCount; rowIndex++) {
+            for (let rowIndex = 2; rowIndex <= Asc.scope.rowCount; rowIndex++) {
                 let row = fillRange.GetRows(rowIndex);
-                for (let columnIndex = 0; columnIndex < Asc.scope.colCount; columnIndex++) {
-                    row.GetCells(columnIndex).SetValue(`Mocked_${rowIndex}_${columnIndex}`);
+                for (let columnIndex = 1; columnIndex <= Asc.scope.colCount; columnIndex++) {
+                    row.GetCells(columnIndex).SetValue(`Mocked_${rowIndex - 1}_${columnIndex}`);
                 }
             }
         })
